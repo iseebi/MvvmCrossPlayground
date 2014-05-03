203de312e9874b19753e15f7c5aca04cd966792b
Nobuhiro Ito <iseebi@iseteki.net>
2014-05-04 03:24:07 +0900

    Add plugin bootstraps

---
+++ MvxImageViewBindingSample/MvxImageViewBindingSample.Droid/Bootstrap/DownloadCachePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace MvxImageViewBindingSample.Droid.Bootstrap
+{
+    public class DownloadCachePluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.DownloadCache.PluginLoader>
+    {
+    }
+}
